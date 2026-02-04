1a7ae51a76166d1dd25347530a2b665a09b76306
Alexander Duncan <3007887d@student.gla.ac.uk>
2026-02-04 18:19:08 +0000

    Chapter 8 Complete

--- rango/views.py
+++ rango/views.py
@@ -18,8 +18,9 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Alexander Duncan'}
-    return render(request, 'rango/about.html', context=context_dict)
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 
 def show_category(request,category_name_slug):
